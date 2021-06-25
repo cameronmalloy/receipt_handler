7490271e6d996218dd37d1cecc89c5f3655ea24b
Cameron Malloy <cmalloy@berkeley.edu>
2021-06-24 17:38:58 -0700

    Loggin

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ def home():
         print('posting')
         img_byte_content = request.form.get('image')
         print('Byte content type:', type(img_byte_content))
+        print(img_byte_content)
         nparr = np.fromstring(img_byte_content, np.uint8)
+        print(len(nparr))
         img_np = cv2.imdecode(nparr, cv2.COLOR_BGR2GRAY)
         return str(img_np)
