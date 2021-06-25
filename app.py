dcb664cbc3f4d843194632f94d19c2de8f2179ef
Cameron Malloy <cmalloy@berkeley.edu>
2021-06-24 17:47:02 -0700

    Fixed quick bug

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ def home():
     if request.method == 'POST':
         print('posting')
         encoded_img = request.form.get('image')
-        print('Byte content type:', type(img_byte_content))
+        print('Byte content type:', type(encoded_img))
         img_byte_content = base64.b64decode(encoded_img)
+        print('Byte content type:', type(img_byte_content))
         nparr = np.fromstring(img_byte_content, np.uint8)
         print(len(nparr))
         img_np = cv2.imdecode(nparr, cv2.COLOR_BGR2GRAY)
