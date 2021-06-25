0b19c9d0a9f547ce8197b6c865eb7678f75ed97a
Cameron Malloy <cmalloy@berkeley.edu>
2021-06-24 17:06:50 -0700

    Added get functionality to app.py

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ import cv2
 
 app = flask.Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    img_byte_content = request.form.get('image')
-    nparr = np.fromstring(img_byte_content, np.uint8)
-    img_np = cv2.imdecode(nparr, cv2.COLOR_BGR2GRAY)
-    return img_np
+    if request.method == 'GET':
+        return 'Hello world!'
+    if request.method == 'POST':
+        img_byte_content = request.form.get('image')
+        nparr = np.fromstring(img_byte_content, np.uint8)
+        img_np = cv2.imdecode(nparr, cv2.COLOR_BGR2GRAY)
+        return img_np
